b01937bb778c21ba0e21c826ba685899cdfb16cd
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-06-24 13:54:39 +0700

    feat: add lossless claw

--- scripts/setup/40-openclaw.sh
+++ scripts/setup/40-openclaw.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 OFFICIAL_VERSION="2026.6.8"
+
+declare -A PACKAGES=(
+  ["@martian-engineering/lossless-claw"]="0.13.1"
+)
+
+for package in "${!PACKAGES[@]}"; do
+  version="${PACKAGES[$package]}"
+  echo "Installing OpenClaw $package@$version..."
+  openclaw plugins install "$package@$version"
+done
